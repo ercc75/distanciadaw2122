e9fae6822774daeb4545a2a1378fd2fa05a7b57c
Eduardo R. Cañizares Caballero <ecanizarescaballero@gmail.com>
2022-05-19 20:18:16 +0200

    Primera version de miProyectoPHP

---
+++ practica-canizares.php
@@ -0,0 +1,33 @@
+<?php
+	function suma($a, $b) {
+		/**
+		* Funcion suma
+		*
+		* La funcion suma realiza la suma de dos numeros pasados por parametro
+		*
+		* @param number $a Numero del primer parametro
+		* @param number $b Numero del segundo parametro
+		* @return number Devuelve un numero que es la suma
+		* @author Eduardo R. Cañizares Caballero
+		* @version 1.0
+		* @internal Contacto del programador: yo@programas.com
+		*/
+		return $a + $b;
+	}
+
+	function resta($a, $b) {
+		/**
+		* Funcion resta
+		*
+		* La funcion resta realiza la resta de dos numeros pasados por parametro
+		*
+		* @param number $a Numero del primer parametro
+		* @param number $b Numero del segundo parametro
+		* @return number Devuelve un numero que es la resta
+		* @author Eduardo R. Cañizares Caballero
+		* @version 1.0
+		* @internal Contacto del programador: yo@programas.com
+		*/
+		return $a - $b;
+	}
+?>
